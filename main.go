f30cc70c502be3e35be1ccdd2af31302397291c7
Avishka Umesh <umesh.avishka@gmail.com>
2023-04-07 13:49:20 +0530

    added functions

--- main.go
+++ main.go
@@ -5,14 +5,29 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Wrote %d bytes", n))
-	})
+const PORT = ":8080"
 
-	http.ListenAndServe(":8080", nil)
+// Home is a function to handle home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World! This is Home Page")
+}
+
+// About is a function to handle about page
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(10, 20)
+	fmt.Fprintf(w, fmt.Sprintf("Hello, World! This is About Page. Sum of 10 and 20 is %d", sum))
+}
+
+// addValues is a function to add two numbers
+func addValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println("Server is running on port", PORT)
+	http.ListenAndServe(PORT, nil)
 }
