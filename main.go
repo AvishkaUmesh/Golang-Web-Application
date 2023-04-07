456339cd73bcc1eb716f55a43743c37dbde95236
Avishka Umesh <umesh.avishka@gmail.com>
2023-04-07 14:12:10 +0530

    added template

--- main.go
+++ main.go
@@ -3,24 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const PORT = ":8080"
 
 // Home is a function to handle home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World! This is Home Page")
+	renderTemplate(w, "home.page.html")
 }
 
 // About is a function to handle about page
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(10, 20)
-	fmt.Fprintf(w, fmt.Sprintf("Hello, World! This is About Page. Sum of 10 and 20 is %d", sum))
+	renderTemplate(w, "about.page.html")
 }
 
-// addValues is a function to add two numbers
-func addValues(x, y int) int {
-	return x + y
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error executing template :", err)
+		return
+	}
+
 }
 
 func main() {
